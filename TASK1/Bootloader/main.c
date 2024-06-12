654a12f44bba6c192b4b0e92a85ea4166f5bb066
TUNGBKHN <nguyenthanhtung8196@gmail.com>
2024-06-13 00:04:25 +0900

    init

---
+++ TASK1/Bootloader/main.c
@@ -0,0 +1,24 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <stdint.h>
+
+
+int main()
+{
+    while(1)
+    {
+        
+    }
+    return 0;
+}
+
+
+
